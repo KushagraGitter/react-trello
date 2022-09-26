50e940f2a87add0d59507528e79413c2b99af182
KushagraGitter <kushagra305@gmail.com>
2022-09-26 23:40:46 +0530

    1 file modified, 1 added

--- src/login.js
+++ src/login.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import './login.css';
 
 const Login = () => {
   return (
-    <form>
-      <div className="email">
-        <label for="email"></label>
-        <input type="email" id="email" placeholder="email address"></input>
-      </div>
-      <div className="password">
-        <label for="password"></label>
-        <input
-          type="password"
-          id="password"
-          placeholder="password address"
-        ></input>
-      </div>
-      <div className="signin">
-        <button className="sign-in">Sign In</button>
-      </div>
-    </form>
+    <div className="login-container">
+      <form className="login-form">
+        <div className="email">
+          <label for="email"></label>
+          <input type="email" id="email" placeholder="email address"></input>
+        </div>
+        <div className="password">
+          <label for="password"></label>
+          <input
+            type="password"
+            id="password"
+            placeholder="password address"
+          ></input>
+        </div>
+        <div className="signin">
+          <button className="sign-in">Sign In</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
